1ec99b53211db983cf3b63bd64ab9dfd5a0a1180
Graheel <graheelupadhyay@gmail.com>
2025-09-07 21:08:00 +0530

    Update DriverLoginForm.js

--- frontend/src/components/DriverLoginForm.js
+++ frontend/src/components/DriverLoginForm.js
@@ -10,7 +10,7 @@ const DriverLoginForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+            const res = await axios.post("https://i-bus-e-ticket-1.onrender.com/api/auth/login", { email, password });
 
             if (res.data.user.role !== "driver") {
                 alert("Not a driver account!");
